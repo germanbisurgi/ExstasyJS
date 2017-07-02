7023ecf8b17fa5b20427716ddf297652c02f3bcc
gerhus <gebisurgi@gmail.com>
2017-07-02 22:23:42 +0200

    updated things :P

--- src/scripts/states/eventState.js
+++ src/scripts/states/eventState.js
@@ -4,8 +4,8 @@ var pointer;
 eventState.create = function () {
 
     circle = this.addCircle(50, 50, 25);
-    pointer = this.addCircle(50, 50, 10);
-    pointer.fill('green');
+    pointer = this.addCircle(50, 50, 1);
+    pointer.fill('transparent');
     
 
     this.addEvent('banana', function () {
@@ -26,6 +26,14 @@ eventState.update = function () {
     pointer.dy = this.game.inputManager.mouse.y - pointer.dh / 2;
 
     if (this.circleCollision(pointer, circle)) {
-        console.log('hover');
+        if (this.touched()) {
+            circle.fill('yellow');
+        }/* else {
+            circle.fill('purple');
+        }*/
+        
+    } else {
+        circle.fill('green');
     }
+
 };
